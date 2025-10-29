c645d4d2c86388afe1a10ec2bf0acd2c9da4e5f9
Dzaka Al <dzakaal10@gmail.com>
2025-10-29 23:21:26 +0700

    fix: Update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Penting untuk GitHub Pages
+  base: '/Portofolio/', // Repository name untuk GitHub Pages
   server: {
     port: 3000,
     open: true
